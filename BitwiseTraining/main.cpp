aea4120c2bba91e365bb261c47286dd52bff3e3d
raMitov <chizmywoe@gmail.com>
2025-03-31 17:42:07 +0300

    tg

--- BitwiseTraining/main.cpp
+++ BitwiseTraining/main.cpp
@@ -36,6 +36,24 @@ int countSetBits(int number) {
     }
     return count;
 }
+//Check if int is a power of two
+bool checkIfPow(int n) {
+    return (n & (n - 1)) == 0;
+}
+//Swap two numbers with bits
+void swap(int &a, int &b) {
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+}
+//Find unique element in an array
+int ReturnUniqueElementInArray(const int *array, const int size) {
+    int unique = 0;
+    for (int i = 0; i < size; i++) {
+        unique ^= array[i];
+    }
+    return unique;
+}
 int main() {
 
     int checkNumber;
@@ -52,7 +70,18 @@ int main() {
     reverseBits(newNumber);
     std::cout << newNumber << std::endl;
     std::cout << countSetBits(checkNumber) << std::endl;
-    return 0;
+    int testInt = 4;
+    std::cout << checkIfPow(testInt) << std::endl;
+
+    std::cout << "The ckeck number is " << checkNumber << std::endl;
+    std::cout << "The test int is" << testInt << std::endl;
+    swap(checkNumber, testInt);
+    std::cout << "The ckeck number is " << checkNumber << std::endl;
+    std::cout << "The test int is" << testInt << std::endl;
+    int arr[] = { 1, 1, 1, 1, 1, 3, 1, 1};
+    int unique = ReturnUniqueElementInArray(arr, sizeof(arr) / sizeof(arr[0]));
+    std::cout << unique << std::endl;
+
 
 }
 
